2e49f842c7781d8900bc333bfb00feab526313e4
Arnar-Sig <enteko5@gmail.com>
2020-10-23 23:51:50 +0000

    Encode fall nokkurn veginn útfært

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@
  */
 
 const LETTERS = `AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ`;
+const LETTERSA = LETTERS.split("");
 
 /**
  * Byrja forrit.
@@ -13,6 +14,10 @@ function start() {
 }
 
 // Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
+
+
+
+
 start();
 let koda_afkoda = '';
 while(koda_afkoda != 'kóða' && koda_afkoda != 'afkóða') {
@@ -22,13 +27,12 @@ while(koda_afkoda != 'kóða' && koda_afkoda != 'afkóða') {
 let hlidrun ='';
 hlidrun = prompt('hliðrun:');
 hlidrun = parseInt(hlidrun);
-console.log(typeof hlidrun);
-console.log(hlidrun);
 
-let texti = prompt(`Gefðu upp strenginn sem á að ${koda_afkoda} með hliðrun ${hlidrun}`)
+let texti = prompt(`Gefðu upp strenginn sem á að ${koda_afkoda} með hliðrun ${hlidrun}`);
+texti = texti.toUpperCase();
+console.log(texti);
 
-
-
+console.log(encode(texti, hlidrun));
 
 
 
@@ -41,14 +45,28 @@ let texti = prompt(`Gefðu upp strenginn sem á að ${koda_afkoda} með hliðrun
  * @returns {string} Upprunalegi strengurinn hliðraður um n til hægri
  */
 function encode(str, n) {
-  let encoded;
   let splittad = str.split("");
-  for(var i = 0; i<splittad.length;i++){              //var for(var i = 0; i<str.length;i++){ 
-
-
+  console.log(splittad);
+ 
+for(var i = 0; i<splittad.length; i++){
+  for (var j = 0; j<LETTERSA.length; j++){
+    if (splittad[i] == LETTERSA[j]){
+      splittad[i] = j
+    }
   }
-
-  return str;
+}
+for (var i = 0; i<splittad.length; i++){
+  splittad[i] = splittad[i] + n;
+  if(splittad[i]>32){
+    splittad[i] = splittad[i] - 32;
+  }
+  
+}
+for (var i = 0; i<splittad.length; i++){
+  splittad[i] = LETTERS[splittad[i]];
+}
+samansett = splittad.join("");
+  return samansett;
 }
 
 /**
@@ -59,12 +77,15 @@ function encode(str, n) {
  * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
  */
 function decode(str, n) {
+
+
   return str;
 }
 
-console.assert(encode('A', 3) === 'D', 'kóðun á A með n=3 er D');
+/*console.assert(encode('A', 3) === 'D', 'kóðun á A með n=3 er D');
 console.assert(decode('D', 3) === 'A', 'afkóðun á D með n=3 er A');
 console.assert(encode('AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ', 32) === 'AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ', 'kóðun með n=32 er byrjunarstrengur');
 console.assert(encode('AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ', 3) === 'DÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖAÁB', 'kóðun á stafrófi með n=3');
 console.assert(decode('DÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖAÁB', 3) === 'AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ', 'afkóðun á stafrófi með n=3');
 console.assert(decode(encode('HALLÓHEIMUR', 13), 13) === 'HALLÓHEIMUR', 'kóðun og afkóðun eru andhverf');
+*/
